945b92714988394ebcdc7e6ca18da6179a3ce40b
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-15 07:09:27 -0700

    fixed broken parsing in search_text fxn

--- R/search_text.r
+++ R/search_text.r
@@ -17,6 +17,9 @@
 #' search_text(text = "Homo sapiens", options = "root")
 #' search_text(text = "Homo sapiens", options = "uri")
 #' search_text(text = "Homo sapiens", options = c("string","type","uri"))
+#' search_text(text = "Homo sapiens", options = c("string","type","uri"), simplify=FALSE) 
+#' 
+#' # pass in curl options
 #' library("httr")
 #' search_text(text = "Homo sapiens", options = "names", config=verbose())
 #' }
@@ -24,6 +27,7 @@
 search_text <- function(text, options="string", simplify=TRUE, ...)
 {
   url <- "http://phylopic.org/api/a/name/search"
+  opts <- length(options)
   options <- paste0(options, collapse = " ")
   args <- phy_compact(list(text = text, options = options))
   tt <- GET(url, query=args, ...)
@@ -32,9 +36,24 @@ search_text <- function(text, options="string", simplify=TRUE, ...)
   res <- content(tt, as = "text")
   out <- fromJSON(res, FALSE)
   if(simplify){
-    unname(unlist(do.call(c, sapply(out$result, function(x) x[[1]], simplify=TRUE))))
+    if(opts == 1){
+      if(options %in% c("string","type","namebankID","root")){ 
+        opt_length <- "mas" 
+      } else if(options %in% "names") {
+        opt_length <- "menos"
+      }
+    } else {
+      opt_length <- "mas"
+    }
+    foo <- function(y){
+      tmp <- data.frame(do.call(rbind, lapply(y, function(x) as.character(x[[1]]))), stringsAsFactors = FALSE)
+      names(tmp) <- names(y[[1]][[1]])
+      tmp
+    }
+    switch(opt_length, 
+           menos = unname(unlist(do.call(c, sapply(out$result, function(x) x[[1]], simplify=TRUE)))),
+           mas = foo(out$result))
   } else {
     out$result    
   }
-#   do.call(c, sapply(out$result, function(x) x[[1]], simplify=TRUE))
 }
