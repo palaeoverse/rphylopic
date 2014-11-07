ff32dd33015a1210081352126cbe827b9448c6f1
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-06 16:22:05 -0800

    removed unneeded parameters in namesets fxns

--- R/name_sets.R
+++ R/name_sets.R
@@ -4,15 +4,6 @@
 #' @param uuid One or more name UUIDs.
 #' @param options (character) One or more of citationStart, html, namebankID, root, string,
 #' type, uid, uri, and/or votes
-#' @param subtaxa If immediate, returns data for immediate subtaxa ("children").
-#' Otherwise, does not include subtaxa.
-#' @param supertaxa If immediate, returns data for immediate supertaxa ("parents").
-#' If all, returns data for all supertaxa ("ancestors"). Otherwise, does not
-#' include supertaxa.
-#' @param other If set to TRUE, includes related taxa in the search.
-#' @param text (character) The text string to search on.
-#' @param useUBio (logical) If TRUE, and there is pending data from uBio that needs to be cached, 
-#' a list of commands will be passed back instead of the normal result.
 #' @param ... Curl options passed on to \code{\link[httr]{GET}}
 #' @details I'm not adding methods for adding names because I can't imagine doing that in R. 
 #' Am I wrong?
