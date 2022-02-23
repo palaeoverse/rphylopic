b7819cf09e8763562e6244f7cb13135077b49af3
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-02-23 11:56:57 -0500

    Make changes requested by author

--- tests/testthat/test-gather_images.R
+++ tests/testthat/test-gather_images.R
@@ -3,8 +3,8 @@ test_that("gather_images works", {
   species <- c("Mus_musculus","Pan_troglodytes","Homo_sapiens")
   res <- rphylopic::gather_images(species=species)
   
-  testthat::expect_equal(species, res$species)
+  testthat::expect_equal(gsub("_"," ",species), res$species)
   testthat::expect_true(
-    all(c("species","uid","namebankID","string","picid") %in% colnames(res))
+    all(c("species","uid","namebankID","species","picid") %in% colnames(res))
   )
 })
