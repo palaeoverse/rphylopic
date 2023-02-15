2c782b79c575305994030be1a748dbf96b39f8cd
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2023-02-15 18:49:08 +0100

    update 'rphylopic-package' and description

--- R/rphylopic-package.R
+++ R/rphylopic-package.R
@@ -1,27 +1,8 @@
-#' rphylopic
-#' 
-#' Get Silhouettes of Organisms from Phylopic
-#'
-#' @importFrom grid popViewport grid.raster pushViewport rasterGrob
-#' @importFrom gridBase baseViewports
-#' @importFrom grDevices col2rgb rgb dev.cur
-#' @importFrom graphics par rasterImage
-#' @importFrom ggplot2 annotation_custom element_text
-#' @importFrom crul HttpClient
-#' @importFrom jsonlite fromJSON
-#' @importFrom png readPNG writePNG
-#' @name rphylopic
-#' @aliases rphylopic-package
-#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
-#' @author David Miller \email{dave@@ninepointeightone.net}
-#' @docType package
-NULL
-
-#' This function is defunct.
-#' @export
-#' @rdname plot_phylopic_base-defunct
 #' @keywords internal
-#' @note see [add_phylopic_base()]
-plot_phylopic_base <- function(...) {
-  .Defunct(msg = "`plot_phylopic_base` is defunct; use `?add_phylopic_base`")
-}
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
