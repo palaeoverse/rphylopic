4bb7afb793bd2d62e9b58533dbcf10bdba24fb4e
Will Gearty <willgearty@gmail.com>
2023-03-08 10:10:45 -0500

    Allow name = NULL and n > 48 for get_uuid

--- R/get_uuid.R
+++ R/get_uuid.R
@@ -4,12 +4,13 @@
 #' for an input taxonomic name. As multiple silhouettes can exist for each
 #' species in PhyloPic, this function extracts the primary image.
 #'
-#' @param name \code{character}. A taxonomic name. Different taxonomic levels
-#'   are supported (i.e. species, genus, family).
+#' @param name \code{character}. A taxonomic name. Various taxonomic levels
+#'   are supported (e.g. species, genus, family). NULL can also be supplied
+#'   which will skip the taxonomic filtering of the PhyloPic database.
 #' @param n \code{numeric}. How many uuids should be returned? Depending
-#' on the requested `name`, multiple silhouettes might exist. If `n` exceeds
-#' the number of available images, all available uuids will be returned. This
-#' argument defaults to 1.
+#'   on the requested `name`, multiple silhouettes might exist. If `n` exceeds
+#'   the number of available images, all available uuids will be returned. This
+#'   argument defaults to 1.
 #' @param url \code{logical}. If \code{FALSE} (default), only the uuid is
 #'   returned. If \code{TRUE}, a valid PhyloPic image url of the uuid is
 #'   returned.
@@ -19,7 +20,7 @@
 #'
 #' @details This function returns uuid(s) or image url (svg) for an input
 #'   \code{name}. If a specific image is desired, the user can make use of
-#'    [pick_phylopic] to visually select the desired uuid/url.
+#'   [pick_phylopic] to visually select the desired uuid/url.
 #' @importFrom stats setNames
 #' @export
 #' @examples
@@ -27,11 +28,8 @@
 #' uuid <- get_uuid(name = "Dinosauria", n = 5, url = TRUE)
 get_uuid <- function(name = NULL, n = 1, url = FALSE) {
   # Error handling -------------------------------------------------------
-  if (is.null(name)) {
-    stop("A taxonomic `name` is required (e.g. Acropora cervicornis).")
-  }
-  if (!is.character(name)) {
-    stop("`name` should be of class character.")
+  if (!is.null(name) && !is.character(name)) {
+    stop("`name` should be `NULL` or of class character.")
   }
   if (!is.numeric(n)) {
     stop("`n` should be of class numeric.")
@@ -43,31 +41,35 @@ get_uuid <- function(name = NULL, n = 1, url = FALSE) {
   name <- tolower(name)
   name <- gsub("_", " ", name)
   # API call -------------------------------------------------------------
-  api_return <- phy_GET("images",
-                        list(embed_items = if (url) "true" else "false",
-                             filter_name = name,
-                             page = 0))
-  # Error handling
-  if ("errors" %in% names(api_return)) {
+  opts <- list()
+  if (!is.null(name)) opts$filter_name <- name
+  api_return <- phy_GET("images", opts)
+  total_items <- api_return$totalItems
+  if (total_items == 0) {
     stop(paste0("Image resource not available for `name`. \n",
                 "Ensure provided name is a valid taxonomic name or ",
                 "try a species/genus resolution name."))
+  } else if (total_items < n) {
+    warning(paste0("Only ", total_items, " items are available."))
+    n <- total_items
   }
-  # Extract uuid ----------------------------------------------------------
-  uuids <- api_return$`_links`$items$href
-  uuids <- sub("/images/", "", uuids)
-  uuids <- sub("(\\?build=\\d+)$", "", uuids)
-  # Update n if greater than available uuids
-  if (n > length(uuids)) {
-    n <- length(uuids)
-  }
-  # Extract n uuids
-  uuids <- uuids[1:n]
-
-  # Build URL? ------------------------------------------------------------
-  if (url) {
-    href <- api_return$`_embedded`$items$`_links`$vectorFile$href
-    uuids <- setNames(href[1:n], uuids)
-  }
-  return(uuids)
+  n_pages <- ceiling(n / api_return$itemsPerPage)
+  opts$embed_items <- if (url) "true" else "false"
+  ret <- lapply(1:n_pages, function(i) {
+    opts$page <- i - 1 # 0-based indexing
+    api_return <- phy_GET("images", opts)
+    # Extract uuids ---------------------------------------------------------
+    uuids <- api_return$`_links`$items$href
+    uuids <- sub("/images/", "", uuids)
+    uuids <- sub("(\\?build=\\d+)$", "", uuids)
+    
+    # Get image URLs --------------------------------------------------------
+    if (url) {
+      href <- api_return$`_embedded`$items$`_links`$vectorFile$href
+      uuids <- setNames(href, uuids)
+    }
+    uuids
+  })
+  ret <- do.call(c, ret)[1:n]
+  return(ret)
 }
--- tests/testthat/test-get_uuid.R
+++ tests/testthat/test-get_uuid.R
@@ -3,10 +3,13 @@ test_that("get_uuid works", {
   # Expect true
   expect_true(is.character(get_uuid(name = "Acropora cervicornis")))
   expect_true(is.character(get_uuid(name = "Tyrannosaurus", url = TRUE)))
-  expect_true(is.character(get_uuid(name = "Acropora", n = 10, url = TRUE)))
+  expect_true(is.character(get_uuid(name = "Acropora", n = 1, url = TRUE)))
+  expect_true(length(get_uuid(name = NULL, n = 100)) == 100)
+  expect_true(length(get_uuid(name = NULL, n = 100, url = TRUE)) == 100)
+  # Expect warnings
+  expect_warning(is.character(get_uuid(name = "Acropora", n = 10, url = TRUE)))
   # Expect errors
   expect_error(get_uuid(name = "Didgeridoo"))
-  expect_error(get_uuid(name = NULL))
   expect_error(get_uuid(name = 1))
   expect_error(get_uuid(name = "Acropora cervicornis", url = 1))
   expect_error(get_uuid(name = "Acropora cervicornis", n = "5"))
