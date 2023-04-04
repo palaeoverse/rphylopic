486173ad78af5728d2d6b87ed4514fdb68d95717
Will Gearty <willgearty@gmail.com>
2023-04-04 17:32:13 -0400

    Increase test coverage

--- tests/testthat/test-geom_phylopic.R
+++ tests/testthat/test-geom_phylopic.R
@@ -8,13 +8,23 @@ test_that("geom_phylopic works", {
                             "c8f71c27-71db-4b34-ac2d-e97fea8762cf",
                             "0a8ab4f9-04c9-4485-b21a-df683d506055"))
   gg <- ggplot(df) +
-    geom_phylopic(aes(x = x, y = y, uuid = uuid), color = "purple", size = 10) +
+    geom_phylopic(aes(x = x, y = y, uuid = uuid), color = "purple", size = 10,
+                  horizontal = TRUE, angle = 45) +
     facet_wrap(~uuid) +
     coord_cartesian(xlim = c(1, 6), ylim = c(5, 30)) +
     theme_classic(base_size = 16)
   expect_true(is.ggplot(gg))
   expect_true(is(gg$layers[[1]]$geom, "GeomPhylopic"))
   expect_doppelganger("geom_phylopic", gg)
+  
+  cat_png <- get_phylopic("23cd6aa4-9587-4a2e-8e26-de42885004c9",
+                          format = "512")
+  gg <- ggplot(df) +
+    geom_phylopic(aes(x = x, y = y), img = list(cat_png),
+                  color = "purple", size = 10) +
+    coord_cartesian(xlim = c(1, 6), ylim = c(5, 30)) +
+    theme_classic(base_size = 16)
+  expect_doppelganger("geom_phylopic with png", gg)
 
   # Errors and warnings
   gg <- ggplot(df) +
