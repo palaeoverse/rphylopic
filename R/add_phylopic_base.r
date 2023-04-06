fbc6e7698798599003e3b06aec2b6e5e8dd99e23
Will Gearty <willgearty@gmail.com>
2023-04-06 11:02:41 -0400

    Vectorize add_phylopic_base

--- R/add_phylopic_base.r
+++ R/add_phylopic_base.r
@@ -1,7 +1,7 @@
 #' Add a PhyloPic to a base R plot
 #'
-#' Specify an existing image, taxonomic name, or PhyloPic uuid to add a
-#' PhyloPic silhouette on top of an existing base R plot.
+#' Specify an existing image, taxonomic name, or PhyloPic uuid to add a PhyloPic
+#' silhouette on top of an existing base R plot.
 #'
 #' @param img A [Picture][grImport2::Picture-class] or png array object, e.g.,
 #'   from using [get_phylopic()].
@@ -17,7 +17,8 @@
 #'   `y` are not specified.
 #' @param alpha \code{numeric}. A value between 0 and 1, specifying the opacity
 #'   of the silhouette (0 is fully transparent, 1 is fully opaque).
-#' @param color \code{character}. Color to plot the silhouette in.
+#' @param color \code{character}. Color to plot the silhouette in. If "original"
+#'   is specified, the original color of the silhouette will be used.
 #' @param horizontal \code{logical}. Should the silhouette be flipped
 #'   horizontally?
 #' @param vertical \code{logical}. Should the silhouette be flipped vertically?
@@ -28,8 +29,13 @@
 #'   position on the plot. If all three of these parameters are unspecified,
 #'   then the silhouette will be plotted to the full height and width of the
 #'   plot. The aspect ratio of the silhouette will always be maintained (even
-#'   when a figure is resized). However, if the plot is resized afterwards, the
-#'   absolute size and/or position of the silhouette may change.
+#'   when a plot is resized). However, if the plot is resized after plotting the
+#'   silhouette, the absolute size and/or position of the silhouette may change.
+#'
+#'   Any argument may be a vector of values if multiple silhouettes should be
+#'   plotted. In this case, all other arguments may also be vectors of values,
+#'   which will be recycled as necessary to the length of the longest vector
+#'   argument.
 #'
 #'   When specifying a horizontal and/or vertical flip **and** a rotation, the
 #'   flip(s) will always occur first. If you would like to customize this
@@ -44,41 +50,34 @@
 #' @export
 #' @examples
 #' # single image
-#' plot(1, 1, type="n", main="A cat")
+#' plot(1, 1, type = "n", main = "A cat")
 #' add_phylopic_base(name = "Cat", x = 1, y = 1, ysize = .4)
 #'
 #' # lots of images using a uuid
 #' posx <- runif(10, 0, 1)
 #' posy <- runif(10, 0, 1)
-#' size <- runif(10, 0.01, 0.2)
+#' size <- runif(10, 0.1, 0.3)
 #' angle <- runif(10, 0, 360)
 #' hor <- sample(c(TRUE, FALSE), 10, TRUE)
 #' ver <- sample(c(TRUE, FALSE), 10, TRUE)
 #' cols <- sample(c("black", "darkorange", "grey42", "white"), 10,
 #'                replace = TRUE)
-#' alpha <- runif(10, 0, 1)
 #'
-#' # Since we are plotting a lot of the same image, we should just save
-#' # the image in our environment first
-#' cat <- get_phylopic("23cd6aa4-9587-4a2e-8e26-de42885004c9")
-#'
-#' plot(posx, posy, type="n", main="A cat herd")
-#' for (i in 1:10) {
-#'   add_phylopic_base(cat, x = posx[i], y = posy[i], ysize = size[i],
-#'                     color = cols[i], angle = angle[i],
-#'                     horizontal = hor[i], vertical = ver[i])
-#' }
+#' plot(posx, posy, type = "n", main = "A cat herd")
+#' add_phylopic_base(uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9",
+#'                   x = posx, y = posy, ysize = size,
+#'                   color = cols, angle = angle,
+#'                   horizontal = hor, vertical = ver)
 #'
 #' # Example using a cat background
+#' cat <- get_phylopic("23cd6aa4-9587-4a2e-8e26-de42885004c9")
 #' # setup plot area
-#' plot(posx, posy, type="n", main="A cat herd, on top of a cat",
-#'      xlim=c(0,1), ylim=c(0,1))
+#' plot(posx, posy, type = "n", main = "A cat herd, on top of a cat",
+#'      xlim = c(0,1), ylim = c(0,1))
 #' # plot background cat
-#' add_phylopic_base(cat, alpha=0.2)
+#' add_phylopic_base(img = cat, alpha=0.2)
 #' # overlay smaller cats
-#' for (i in 1:10) {
-#'   add_phylopic_base(cat, x = posx[i], y = posy[i], ysize = size[i], alpha=.8)
-#' }
+#' add_phylopic_base(img = cat, x = posx, y = posy, ysize = size, alpha=.8)
 add_phylopic_base <- function(img = NULL, name = NULL, uuid = NULL,
                               x = NULL, y = NULL, ysize = NULL,
                               alpha = 1, color = "black",
@@ -90,30 +89,52 @@ add_phylopic_base <- function(img = NULL, name = NULL, uuid = NULL,
   if (sum(sapply(list(img, name, uuid), is.null)) < 2) {
     stop("Only one of `img`, `name`, or `uuid` may be specified")
   }
-  if (alpha > 1 || alpha < 0) {
+  if (any(alpha > 1 | alpha < 0)) {
     stop("`alpha` must be between 0 and 1.")
   }
+
   if (!is.null(name)) {
     if (!is.character(name)) {
       stop("`name` should be of class character.")
     }
-    url <- get_uuid(name = name, url = TRUE)
-    if (is.na(url)) {
-      stop("`name` returned no PhyloPic results.")
-    }
-    img <- get_svg(url)
+    # Get PhyloPic for each unique name
+    name_unique <- unique(name)
+    imgs <- sapply(name_unique, function(x) {
+      url <- tryCatch(get_uuid(name = x, url = TRUE),
+                      error = function(cond) NA)
+      if (is.na(url)) {
+        warning(paste0("`name` ", '"', x, '"',
+                       " returned no PhyloPic results."))
+        return(NULL)
+      }
+      get_svg(url)
+    })
+    imgs <- imgs[name]
   } else if (!is.null(uuid)) {
     if (!is.character(uuid)) {
       stop("`uuid` should be of class character.")
     }
-    img <- get_phylopic(uuid)
-  } else if (!is(img, "Picture") && !is.array(img)) {
-    stop("`img` should be of class Picture (for a vector image) or class array
-          (for a raster image).")
+    # Get PhyloPic for each unique uuid
+    uuid_unique <- unique(uuid)
+    imgs <- sapply(uuid_unique, function(x) {
+      img <- tryCatch(get_phylopic(x),
+                      error = function(cond) NULL)
+      if (is.null(img)) {
+        warning(paste0('"', x, '"', " is not a valid PhyloPic `uuid`."))
+      }
+      img
+    })
+    imgs <- imgs[uuid]
+  } else {
+    if (!is.list(img)) img <- list(img)
+    if (any(sapply(img, function(x) {
+      !is(x, "Picture") && !is.array(x)
+    }))) {
+      stop(paste("`img` should be of class Picture (for a vector image)",
+                 "or class array (for a raster image)."))
+    }
+    imgs <- img
   }
-
-  if (horizontal || vertical) img <- flip_phylopic(img, horizontal, vertical)
-  if (!is.null(angle) && angle != 0) img <- rotate_phylopic(img, angle)
 
   # get plot limits
   usr <- par()$usr
@@ -143,17 +164,26 @@ add_phylopic_base <- function(img = NULL, name = NULL, uuid = NULL,
   y <- (y - ylims[1]) / diff(ylims)
   ysize <- ysize / abs(diff(ylims))
 
-  if (is(img, "Picture")) { # svg
-    gp_fun <- function(pars) {
-      if (!is.null(color)) {
-        pars$fill <- color
+  tmp <- mapply(function(img, x, y, ysize, alpha, color, horizontal, vertical, angle) {
+    if (horizontal || vertical) img <- flip_phylopic(img, horizontal, vertical)
+    if (angle != 0) img <- rotate_phylopic(img, angle)
+
+    # grobify (and recolor if necessary)
+    color <- if (color == "original") NULL else color
+    if (is(img, "Picture")) { # svg
+      gp_fun <- function(pars) {
+        if (!is.null(color)) {
+          pars$fill <- color
+        }
+        pars$alpha <- alpha
+        pars
       }
-      pars$alpha <- alpha
-      pars
+      grid.picture(img, x = x, y = y, height = ysize, gpFUN = gp_fun,
+                   expansion = 0)
+    } else { # png
+      img <- recolor_phylopic(img, alpha, color)
+      grid.raster(img, x = x, y = y, height = ysize)
     }
-    grid.picture(img, x = x, y = y, height = ysize, gpFUN = gp_fun)
-  } else { # png
-    img <- recolor_phylopic(img, alpha, color)
-    grid.raster(img, x = x, y = y, height = ysize)
-  }
+  }, img = imgs, x = x, y = y, ysize = ysize, alpha = alpha, color = color,
+     horizontal = horizontal, vertical = vertical, angle = angle)
 }
