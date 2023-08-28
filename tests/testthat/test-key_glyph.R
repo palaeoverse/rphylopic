0860ab8d7272aab35eae2b46998517fda0ee4f44
William Gearty <willgearty@gmail.com>
2023-08-28 10:50:06 -0400

    Fix glyph tests

--- tests/testthat/test-key_glyph.R
+++ tests/testthat/test-key_glyph.R
@@ -1,4 +1,5 @@
 test_that("phylopic_key_glyph works", {
+  library(grid)
   skip_if_offline(host = "api.phylopic.org")
   df <- data.frame(x = c(2, 4), y = c(10, 20),
                    name = c("Felis silvestris catus", "Odobenus rosmarus"))
@@ -9,7 +10,13 @@ test_that("phylopic_key_glyph works", {
     coord_cartesian(xlim = c(1, 6), ylim = c(5, 30)) +
     theme_classic(base_size = 16)
   expect_true(is.ggplot(gg))
-  expect_doppelganger("phylopic_key_glyph", gg)
+  # no idea why, but this rigmarole is required for this test
+  gg_test <- ggplot_build(gg)
+  expect_doppelganger("phylopic_key_glyph",
+                      grid.draw(ggplot_gtable(gg_test)))
   gg <- gg + theme(legend.key.size = grid::unit(5, "lines"))
-  expect_doppelganger("phylopic_key_glyph with larger glyphs", gg)
+  # no idea why, but this rigmarole is required for this test
+  gg_test <- ggplot_build(gg)
+  expect_doppelganger("phylopic_key_glyph with larger glyphs",
+                      grid.draw(ggplot_gtable(gg_test)))
 })
