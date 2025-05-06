d63aec8f51a86ca8980b200aa123bafb30a27b2c
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2025-05-06 20:54:02 +0100

    Add silhouettes to phylogenetic tree (#110)
    * add_phylopic_tree()
    
    * Fail gracefully if tip label missing
    
    * Add add_phylopic_tree to pkgdown.yml
    
    * Support vectorized input
    
    * Leave plotting the tree to the user
    
    * Check that a graphics device is open
    
    But leave it to the user to make sure it's the right one!
    
    * Fail test correctly
    
    * Update add_phylopic_tree.Rd
    
    * rm unneeded comments
    
    * Default `tip` to vector name
    
    * Emphasize base R
    
    * @inheritParams add_phylopic_*base*
    
    * Update add_phylopic_tree.Rd
    
    * import dev.cur from grDevices
    
    * Check graphics device is off
    
    Passing locally but not on CI
    
    * padding & relPadding

---
+++ R/add_phylopic_tree.R
@@ -0,0 +1,99 @@
+#' Add PhyloPics to a phylogenetic tree plotted with base R
+#'
+#' Specify existing images, taxonomic names, or PhyloPic uuids to add PhyloPic
+#' silhouettes alongside the associated leaves of a phylogenetic tree that has
+#' been plotted in the active graphics device using the base R graphics
+#' functions.
+#'
+#' @inheritParams add_phylopic_base
+#' @param tree The phylogenetic tree object of class `phylo` on which to add
+#' the silhouette.
+#' @param tip The tip labels against which to add the silhouettes.
+#' If not specified, the names of the `img`, `uuid` or `name` vector are used.
+#' @param relWidth The width of each silhouette relative to the plotting area.
+#' @param padding,relPadding Distance to inset each silhouette from the right
+#' edge of the plotting area,
+#' in the plot coordinate system (`padding`) or
+#' relative to the size of the plotting area (`relPadding`).
+#' Negative values offset to the right.
+#' @param \dots Further arguments to pass to `add_phylopic_base()`.
+#' @author [Martin R. Smith](https://orcid.org/0000-0001-5660-1727) 
+#' (<martin.smith@durham.ac.uk>)
+#' @seealso
+#' For trees plotted using \pkg{ggtree}, see [`geom_phylopic()`].
+#' @importFrom ape plot.phylo .PlotPhyloEnv
+#' @importFrom grDevices dev.cur
+#' @export
+#' @examples \dontrun{
+#'  # Load the ape library to work with phylogenetic trees
+#' library("ape")
+#' 
+#' # Read a phylogenetic tree
+#' tree <- ape::read.tree(text = "(cat, (dog, mouse));")
+#' 
+#' # Set a large right margin to accommodate the silhouettes
+#' par(mar = c(1, 1, 1, 10))
+#' 
+#' # Plot the tree
+#' plot(tree)
+#' 
+#' # Add a PhyloPic silhouette of a cat to the tree
+#' add_phylopic_tree(
+#'   tree, # Must be the tree that was plotted
+#'   "cat", # Which leaf should the silhouette be plotted against?
+#'   uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9", # Silhouette to plot
+#'   relWidth = 0.2,
+#'   fill = "brown"
+#' )
+#' }
+add_phylopic_tree <- function(tree, tip = names(img) %||% names(uuid) %||%
+                                names(name) %||% name,
+                              img = NULL,
+                              name = if (is.null(img) && is.null(uuid)) tip 
+                                else NULL, 
+                              uuid = NULL, width, padding = NULL,
+                              relWidth = 0.06, relPadding = 1/200,
+                              hjust = 0,
+                              ...) {
+  if (dev.cur() < 2) {
+    # It would be nice to check whether the plotting device that contains
+    # last_plot.phylo is still the active device, but this is not possible - so
+    # we leave the responsibility of calling the function sensibly to the user.
+    stop("No plotting device is open; try plot(tree)")
+  }
+  tipLabels <- tree[["tip.label"]]
+  leafIndex <- match(tip, tipLabels)
+  leafMissing <- is.na(leafIndex)
+  if (any(leafMissing)) {
+    nearMiss <- unlist(lapply(tip[leafMissing], agrep, tipLabels,
+                              max.distance = 0.5), use.names = FALSE,
+                       recursive = FALSE)
+    stop("Could not find '", paste(tip[leafMissing], collapse = "', '"),
+                                   "' in tree$tip.label.  ",
+         if (length(nearMiss)) {
+           paste0("Did you mean '",
+                  paste(tipLabels[nearMiss], collapse = "', '"), "'?")
+         })
+  }
+  coords <- tryCatch(get("last_plot.phylo", envir = .PlotPhyloEnv),
+                     error = function(e) {
+                       stop("plot(tree) has not been called")
+                     })
+  rightEdge <- par("usr")[[2]]
+  leftEdge <- par("usr")[[1]]
+  plotWidth <- rightEdge - leftEdge
+  if (missing(width)) {
+    width <- plotWidth * relWidth
+  }
+  padX <- padding %||% relPadding * plotWidth
+  add_phylopic_base(
+    img = img,
+    name = name,
+    uuid = uuid,
+    x = rightEdge - width - padX,
+    y = coords[["yy"]][leafIndex],
+    hjust = hjust,
+    width = width,
+    ...
+  )
+}
---
+++ tests/testthat/test-add_phylopic_tree.R
@@ -0,0 +1,131 @@
+test_that("add_phylopic_tree works", {
+  skip_if_offline(host = "api.phylopic.org")
+  
+  if (dev.cur() > 1) {
+    # Ensure graphics device is off
+    dev.off()
+  }
+  expect_error(
+    add_phylopic_tree(
+      ape::read.tree(text = "(cat, mouse);"),
+      "mouse",
+      uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9", # Silhouette to plot
+      fill = "red"
+    ),
+    "No plotting device is open"
+  )
+  
+  expect_doppelganger("Add individual silhouettes to tree", function() {
+    # Load the ape library to work with phylogenetic trees
+    library("ape")
+    
+    # Read a phylogenetic tree
+    tree <- ape::read.tree(text = "(cat, (dog, mouse));")
+    
+    # Fail if no tree has (ever) been plotted
+    plot.new() # Open a graphics device without plotting a tree
+    expect_error(add_phylopic_tree(
+      tree,
+      "cat",
+      uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9",
+      relWidth = 0.1,
+      vjust = 0.2,
+      fill = "brown"
+    ), "plot\\(tree\\) has not been called")
+    
+    
+    # Set edge of plot = edge of device
+    par(mar = rep(0, 4))
+    plot(tree)
+    
+    # Add a PhyloPic silhouette of a cat to the tree
+    add_phylopic_tree(
+      tree,
+      "cat",
+      uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9",
+      relWidth = 0.1,
+      padding = 0.01,
+      vjust = 0.2,
+      fill = "brown"
+    )
+    
+    # Add a mouse, by taxon name
+    expect_warning(
+      add_phylopic_tree(
+        tree,
+        "mouse",
+        name = "mus",
+        relWidth = 0.2,
+        relPadding = 0.8, # Should appear flush with left margin
+        fill = "lightblue",
+        vjust = 1
+      ),
+      "used the .name. argument"
+    )
+    
+    # Add a dog, by name
+    expect_warning(
+      add_phylopic_tree(
+        tree,
+        "dog",
+        relWidth = 0.16,
+        padding = -0.08, # Half off the page
+        fill = "#665566"
+      ),
+      "used the .name. argument"
+    )
+    
+    # Fail gracefully if taxon missing
+    expect_error(
+      add_phylopic_tree(
+        tree,
+        "maus", # Should throw error
+        uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9", # Silhouette to plot
+        fill = "red"
+      ),
+      "Could not find 'maus' in tree.tip.label.  Did you mean 'mouse'\\?"
+    )
+  })
+
+  expect_doppelganger("Add vector of silhouettes to tree", function() {
+    # Load the ape library to work with phylogenetic trees
+    library("ape")
+    
+    # Read a phylogenetic tree
+    tree <- ape::read.tree(text = "(cat, (dog, mouse));")
+  
+    # Set edge of plot = edge of device
+    par(mar = rep(0, 4)) 
+    plot(tree)
+    
+    # Add a vector of silhouettes to the tree
+    add_phylopic_tree(
+      tree = tree,
+      uuid = c(cat = "23cd6aa4-9587-4a2e-8e26-de42885004c9",
+               mouse = "dd0a795e-4be3-4f99-a084-2427c1319d31",
+               dog = "6f3ebbc6-be53-4216-b45b-946f7984669b"),
+      relWidth = c(0.1, 0.2, 0.16),
+      padding = c(1/200, 0.8, -0.08),
+      vjust = c(0.2, 1, 1),
+      fill = c("brown", "lightblue", "#665566"),
+    )
+    
+    # Check leaf validation
+    expect_error(add_phylopic_tree(
+      tree = tree,
+      tip = c("cat", "mouse", "dawg", "maus", "non-existent taxon"),
+      uuid = c("23cd6aa4-9587-4a2e-8e26-de42885004c9",
+               "dd0a795e-4be3-4f99-a084-2427c1319d31",
+               "6f3ebbc6-be53-4216-b45b-946f7984669b",
+               "dd0a795e-4be3-4f99-a084-2427c1319d31",
+               "dd0a795e-4be3-4f99-a084-2427c1319d31"
+               ),
+      relWidth = c(0.1, 0.2, 0.16, 9, 9),
+      padding = c(1/200, 0.8, -0.08, 0, 0),
+      vjust = c(0.2, 1, 1, 0.5, 0.5),
+      fill = c("brown", "lightblue", "#665566", "red", "red"),
+      ),
+    "Could not find 'dawg', 'maus', 'no.+on' in tree.+ mean 'dog', 'mouse'\\?"
+    )
+  })
+})
