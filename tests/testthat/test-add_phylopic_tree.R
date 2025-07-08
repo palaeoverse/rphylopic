c599f395059fdc2cca40e8218c680768cac9813c
William Gearty <willgearty@gmail.com>
2025-07-08 17:28:10 -0400

    Only run add_phylopic_tree doppelganger tests on Windows

--- tests/testthat/test-add_phylopic_tree.R
+++ tests/testthat/test-add_phylopic_tree.R
@@ -1,5 +1,8 @@
 test_that("add_phylopic_tree works", {
   skip_if_offline(host = "api.phylopic.org")
+  skip_if_not_installed("ape")
+  # Load the ape library to work with phylogenetic trees
+  suppressPackageStartupMessages(require(ape, quietly = TRUE))
   
   if (dev.cur() > 1) {
     # Ensure graphics device is off
@@ -15,30 +18,65 @@ test_that("add_phylopic_tree works", {
     "No plotting device is open"
   )
   
-  expect_doppelganger("Add individual silhouettes to tree", function() {
-    # Load the ape library to work with phylogenetic trees
-    library("ape")
-    
-    # Read a phylogenetic tree
-    tree <- ape::read.tree(text = "(cat, (dog, mouse));")
-    
-    # Fail if no tree has (ever) been plotted
-    plot.new() # Open a graphics device without plotting a tree
-    expect_error(add_phylopic_tree(
+  # Read a phylogenetic tree
+  tree <- ape::read.tree(text = "(cat, (dog, mouse));")
+  
+  # Fail if no tree has (ever) been plotted
+  plot.new() # Open a graphics device without plotting a tree
+  expect_error(add_phylopic_tree(
+    tree,
+    "cat",
+    uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9",
+    relWidth = 0.1,
+    vjust = 0.2,
+    fill = "brown"
+  ), "plot\\(tree\\) has not been called")
+  
+  plot(tree)
+  # Add a mouse, by taxon name
+  expect_warning(
+    add_phylopic_tree(
       tree,
-      "cat",
-      uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9",
-      relWidth = 0.1,
-      vjust = 0.2,
-      fill = "brown"
-    ), "plot\\(tree\\) has not been called")
-    
-    
+      "mouse",
+      name = "mus",
+      relWidth = 0.2,
+      relPadding = 0.8, # Should appear flush with left margin
+      fill = "lightblue",
+      vjust = 1
+    ),
+    "used the .name. argument"
+  )
+  
+  # Add a dog, by name
+  expect_warning(
+    add_phylopic_tree(
+      tree,
+      "dog",
+      relWidth = 0.16,
+      padding = -0.08, # Half off the page
+      fill = "#665566"
+    ),
+    "used the .name. argument"
+  )
+  
+  # Fail gracefully if taxon missing
+  expect_error(
+    add_phylopic_tree(
+      tree,
+      "maus", # Should throw error
+      uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9", # Silhouette to plot
+      fill = "red"
+    ),
+    "Could not find 'maus' in tree.tip.label.  Did you mean 'mouse'\\?"
+  )
+  
+  skip_if(R.Version()$os != "mingw32") # only test the rest on Windows
+  expect_doppelganger("Add individual silhouettes to tree", function() {
     # Set edge of plot = edge of device
     par(mar = rep(0, 4))
     plot(tree)
     
-    # Add a PhyloPic silhouette of a cat to the tree
+    # Add PhyloPic silhouette to the tree
     add_phylopic_tree(
       tree,
       "cat",
@@ -48,52 +86,54 @@ test_that("add_phylopic_tree works", {
       vjust = 0.2,
       fill = "brown"
     )
-    
-    # Add a mouse, by taxon name
-    expect_warning(
-      add_phylopic_tree(
-        tree,
-        "mouse",
-        name = "mus",
-        relWidth = 0.2,
-        relPadding = 0.8, # Should appear flush with left margin
-        fill = "lightblue",
-        vjust = 1
-      ),
-      "used the .name. argument"
+    add_phylopic_tree(
+      tree,
+      "mouse",
+      uuid = "dd0a795e-4be3-4f99-a084-2427c1319d31",
+      relWidth = 0.2,
+      relPadding = 0.8, # Should appear flush with left margin
+      fill = "lightblue",
+      vjust = 1
     )
-    
-    # Add a dog, by name
-    expect_warning(
-      add_phylopic_tree(
-        tree,
-        "dog",
-        relWidth = 0.16,
-        padding = -0.08, # Half off the page
-        fill = "#665566"
-      ),
-      "used the .name. argument"
-    )
-    
-    # Fail gracefully if taxon missing
-    expect_error(
-      add_phylopic_tree(
-        tree,
-        "maus", # Should throw error
-        uuid = "23cd6aa4-9587-4a2e-8e26-de42885004c9", # Silhouette to plot
-        fill = "red"
-      ),
-      "Could not find 'maus' in tree.tip.label.  Did you mean 'mouse'\\?"
+    add_phylopic_tree(
+      tree,
+      "dog",
+      uuid = "6f3ebbc6-be53-4216-b45b-946f7984669b",
+      relWidth = 0.16,
+      padding = -0.08, # Half off the page
+      fill = "#665566"
     )
   })
+})
 
-  expect_doppelganger("Add vector of silhouettes to tree", function() {
-    # Load the ape library to work with phylogenetic trees
-    library("ape")
-    
-    # Read a phylogenetic tree
-    tree <- ape::read.tree(text = "(cat, (dog, mouse));")
+test_that("add_phylopic_tree works with vectors", {
+  skip_if_offline(host = "api.phylopic.org")
+  skip_if_not_installed("ape")
   
+  # Read a phylogenetic tree
+  tree <- ape::read.tree(text = "(cat, (dog, mouse));")
+  
+  plot(tree)
+  # Check leaf validation
+  expect_error(add_phylopic_tree(
+    tree = tree,
+    tip = c("cat", "mouse", "dawg", "maus", "non-existent taxon"),
+    uuid = c("23cd6aa4-9587-4a2e-8e26-de42885004c9",
+             "dd0a795e-4be3-4f99-a084-2427c1319d31",
+             "6f3ebbc6-be53-4216-b45b-946f7984669b",
+             "dd0a795e-4be3-4f99-a084-2427c1319d31",
+             "dd0a795e-4be3-4f99-a084-2427c1319d31"
+    ),
+    relWidth = c(0.1, 0.2, 0.16, 9, 9),
+    padding = c(1/200, 0.8, -0.08, 0, 0),
+    vjust = c(0.2, 1, 1, 0.5, 0.5),
+    fill = c("brown", "lightblue", "#665566", "red", "red"),
+  ),
+  "Could not find 'dawg', 'maus', 'no.+on' in tree.+ mean 'dog', 'mouse'\\?"
+  )
+  
+  skip_if(R.Version()$os != "mingw32") # only test the rest on Windows
+  expect_doppelganger("Add vector of silhouettes to tree", function() {
     # Set edge of plot = edge of device
     par(mar = rep(0, 4)) 
     plot(tree)
@@ -108,24 +148,6 @@ test_that("add_phylopic_tree works", {
       padding = c(1/200, 0.8, -0.08),
       vjust = c(0.2, 1, 1),
       fill = c("brown", "lightblue", "#665566"),
-    )
-    
-    # Check leaf validation
-    expect_error(add_phylopic_tree(
-      tree = tree,
-      tip = c("cat", "mouse", "dawg", "maus", "non-existent taxon"),
-      uuid = c("23cd6aa4-9587-4a2e-8e26-de42885004c9",
-               "dd0a795e-4be3-4f99-a084-2427c1319d31",
-               "6f3ebbc6-be53-4216-b45b-946f7984669b",
-               "dd0a795e-4be3-4f99-a084-2427c1319d31",
-               "dd0a795e-4be3-4f99-a084-2427c1319d31"
-               ),
-      relWidth = c(0.1, 0.2, 0.16, 9, 9),
-      padding = c(1/200, 0.8, -0.08, 0, 0),
-      vjust = c(0.2, 1, 1, 0.5, 0.5),
-      fill = c("brown", "lightblue", "#665566", "red", "red"),
-      ),
-    "Could not find 'dawg', 'maus', 'no.+on' in tree.+ mean 'dog', 'mouse'\\?"
     )
   })
 })
