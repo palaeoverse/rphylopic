fb06d6a52440da18b72b6507f493bca751454630
William Gearty <willgearty@gmail.com>
2026-05-19 16:02:01 -0400

    Clean up test uuids

--- tests/testthat/helper-cache.R
+++ tests/testthat/helper-cache.R
@@ -2,4 +2,3 @@
 key_exists <- function(key) {
   exists(key, envir = .phy_cache, inherits = FALSE)
 }
-id <- "f7a09e0a-7c42-4c68-adf9-25869322b811"
--- tests/testthat/test-caching.R
+++ tests/testthat/test-caching.R
@@ -2,11 +2,11 @@ test_that("get_phylopic cache makes second call faster", {
   skip_if_offline(host = phost())
   skip_on_cran()
   
-  id <- "f7a09e0a-7c42-4c68-adf9-25869322b811"
+  jay_id <- "f7a09e0a-7c42-4c68-adf9-25869322b811"
   
   # The parsed Picture is keyed by URL in .phy_cache
   # the API response is held in httpcache
-  svg_key <- sprintf("https://images.phylopic.org/images/%s/vector.svg", id)
+  svg_key <- sprintf("https://images.phylopic.org/images/%s/vector.svg", jay_id)
   
   # Clear both caches to ensure a clean first call
   clear_phylopic_cache()
@@ -15,7 +15,7 @@ test_that("get_phylopic cache makes second call faster", {
   
   # First call: populates httpcache (SVG bytes response) and
   # .phy_cache (the parsed Picture object)
-  t1 <- system.time(jay1 <- get_phylopic(id))[["elapsed"]]
+  t1 <- system.time(jay1 <- get_phylopic(jay_id))[["elapsed"]]
   expect_true(httpcache::hitCache(svg_key))
   expect_true(key_exists(svg_key))
   
@@ -26,7 +26,7 @@ test_that("get_phylopic cache makes second call faster", {
     GET = function(...) stop("network access not expected on cached call"),
     .package = "httr"
   )
-  t2 <- system.time(jay2 <- get_phylopic(id))[["elapsed"]]
+  t2 <- system.time(jay2 <- get_phylopic(jay_id))[["elapsed"]]
   expect_identical(jay1, jay2)
   expect_lt(t2, t1 * 0.2)
 })
@@ -35,13 +35,13 @@ test_that("get_phylopic raster cache makes second call faster", {
   skip_if_offline(host = phost())
   skip_on_cran()
   
-  id <- "f7a09e0a-7c42-4c68-adf9-25869322b811"
+  jay_id <- "f7a09e0a-7c42-4c68-adf9-25869322b811"
   height <- 512  # default height for get_phylopic raster format
   
   # make_png keys parsed PNG arrays by URL + height
-  png_key <- sprintf("https://images.phylopic.org/images/%s/vector.svg", id)
+  png_key <- sprintf("https://images.phylopic.org/images/%s/vector.svg", jay_id)
   png_height_key <- sprintf("https://images.phylopic.org/images/%s/vector.svg?h=%s",
-                            id, height)
+                            jay_id, height)
   
   # Clear both caches to ensure a clean first call
   clear_phylopic_cache()
@@ -51,7 +51,7 @@ test_that("get_phylopic raster cache makes second call faster", {
   # First call: populates httpcache (SVG bytes response) and
   # .phy_cache (the parsed PNG array)
   t1 <- system.time(
-    jay1 <- get_phylopic(id, format = "raster", height = height)
+    jay1 <- get_phylopic(jay_id, format = "raster", height = height)
   )[["elapsed"]]
   expect_true(httpcache::hitCache(png_key))
   expect_true(key_exists(png_height_key))
@@ -63,7 +63,7 @@ test_that("get_phylopic raster cache makes second call faster", {
     .package = "httr"
   )
   t2 <- system.time(
-    jay2 <- get_phylopic(id, format = "raster", height = height)
+    jay2 <- get_phylopic(jay_id, format = "raster", height = height)
   )[["elapsed"]]
   expect_identical(jay1, jay2)
   expect_lt(t2, t1 * 0.2)
@@ -73,19 +73,19 @@ test_that("get_phylopic raster cache is keyed per height", {
   skip_if_offline(host = phost())
   skip_on_cran()
   
-  id <- "f7a09e0a-7c42-4c68-adf9-25869322b811"
+  jay_id <- "f7a09e0a-7c42-4c68-adf9-25869322b811"
   
   # make_png keys parsed PNG arrays by URL + height, so the same
-  png_key <- sprintf("https://images.phylopic.org/images/%s/vector.svg", id)
+  png_key <- sprintf("https://images.phylopic.org/images/%s/vector.svg", jay_id)
   png_key_height <- function(h) {
-    sprintf("https://images.phylopic.org/images/%s/vector.svg?h=%s", id, h)
+    sprintf("https://images.phylopic.org/images/%s/vector.svg?h=%s", jay_id, h)
   }
   
   clear_phylopic_cache()
   
   # First call at height 512: populates .phy_cache for that key only,
   # and populates httpcache for the SVG bytes
-  jay512 <- get_phylopic(id, format = "raster", height = 512)
+  jay512 <- get_phylopic(jay_id, format = "raster", height = 512)
   expect_true(httpcache::hitCache(png_key))
   expect_true(key_exists(png_key_height(512)))
   expect_false(key_exists(png_key_height(256)))
@@ -98,7 +98,7 @@ test_that("get_phylopic raster cache is keyed per height", {
     .package = "httr"
   )
   t_diff <- system.time(
-    jay256 <- get_phylopic(id, format = "raster", height = 256)
+    jay256 <- get_phylopic(jay_id, format = "raster", height = 256)
   )[["elapsed"]]
   expect_true(key_exists(png_key_height(256)))
   expect_true(key_exists(png_key_height(512)))  # original entry preserved
@@ -112,6 +112,6 @@ test_that("get_phylopic raster cache is keyed per height", {
     rsvg_png = function(...) stop("rasterization not expected on cached call"),
     .package = "rsvg"
   )
-  jay256_repeat <- get_phylopic(id, format = "raster", height = 256)
+  jay256_repeat <- get_phylopic(jay_id, format = "raster", height = 256)
   expect_identical(jay256, jay256_repeat)
 })
